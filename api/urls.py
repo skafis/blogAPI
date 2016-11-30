dd1b3770e084589e6e01a7172f5904320f3ad605
frank <franciskipchumba5@gmail.com>
2016-11-30 16:37:54 +0300

    add new app view and urls

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib.admin
+from . views import (
+	PostListAPIView,
+	)
+
+urlpatterns = [
+    url(r'^$', PostListAPIView.as_view(), name='post_list'),
+    # url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    # url(r'^post/new/$', views.post_new, name='post_new'),
+    # url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit')
+]
---
+++ api/views.py
@@ -0,0 +1,6 @@
+# from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from . models import Post
+# Create your views here.
+
+class PostListAPIView
