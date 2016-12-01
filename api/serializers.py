e60f0dcb10faa48685b91b7f80a953ed9c8c1f8f
frank <franciskipchumba5@gmail.com>
2016-12-02 00:34:03 +0300

    add serilizer methods for user, images and markdown

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,8 @@
-from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
+from rest_framework.serializers import (
+	ModelSerializer,
+	HyperlinkedIdentityField,
+	SerializerMethodField
+	)
 from .models import Post
 
 post_detail_url = HyperlinkedIdentityField(
@@ -17,21 +21,45 @@ class PostCreateUpdateSerializer(ModelSerializer):
 
 class PostListSerializer(ModelSerializer):
 	url = post_detail_url
+	user = SerializerMethodField()
+
 	class Meta:
 		model = Post
 		fields = [
 		'url',
+		'user',
 		'title',
 		'content',
 		'published_date',
 		]
 
+	def get_user(self, instance):
+		return str(instance.author.username)
+
 class PostDetailSerializer(ModelSerializer):
+	user = SerializerMethodField()
+	image = SerializerMethodField()
+	# markdown = SerializerMethodField()
 	class Meta:
 		model = Post
 		fields = [
-		'author',
+		'user',
 		'title',
 		'content',
+		# 'markdown',
 		'published_date',
+		'image'
 		]
+
+	def get_user(self, instance):
+		return str(instance.author.username)
+
+	def get_image(self, instance):
+		try:
+			image = instance.image.url
+		except:
+			image = None
+		return image
+
+	# def get_html(self, instance):
+	# 	return instance.get_markdown()
